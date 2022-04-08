afbeedd88b586dfb6f99e244d5681fb20e563e7c
gyeomjunchoi <gbeovhsqhtka@naver.com>
2022-04-08 09:50:08 +0900

    *스켈레톤작업완료

---
+++ ExamTDD/src/test/java/com/nhnacademy/exam/test/ParkTest.java
@@ -0,0 +1,12 @@
+package com.nhnacademy.exam.test;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ParkTest {
+
+    @DisplayName("")
+    @Test
+    void name() {
+    }
+}
